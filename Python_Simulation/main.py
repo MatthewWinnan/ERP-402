160b8dc8e4df5b29914a136cb2c6381a6fdac3ac
Matthew <u18037497@tuks.co.za>
2021-11-04 15:16:14 +0200

    Basics done. TODO add hallo manager+RRER, RREP, RREQ and loadbalance.

--- Python_Simulation/main.py
+++ Python_Simulation/main.py
@@ -29,7 +29,7 @@ TOA = T_preamble+T_payload
 amount_symbols = int(1/TOA)
 
 #Define client packet frequency
-packet_freq = 1
+packet_freq = 2
 #Define hallo frequency
 hallo_freq = 1
 
@@ -89,6 +89,8 @@ HALLO_rx = [[0,0],[0,0,0],[0,0,0],[0,0]]
 MESSAGE_rx = [0,0,0,0]
 #Defines when client sent message
 MESSAGE_tx = [0]
+#Defines when destination received message
+DEST_MESSAGE_rx = [0]
 #Defines how many messages I actually sent
 NODE_tx = [0,0,0,0]
 #Defines messages I actually received
@@ -137,6 +139,8 @@ NODE_ETX_value = []
 
 if __name__ == "__main__":
     #Initialize measurement arrays
+    #This will keep track of a change in message receive and reply
+    MESSAGE_RX_PREV = list(np.copy(MESSAGE_rx))
     # Store the loads here
     LOAD_array = []
     for i in range(0,len(NODE_index)):
@@ -195,11 +199,47 @@ if __name__ == "__main__":
         #Initialize the events each second
         for j in range(0,amount_symbols):
             #Here we will propagate the sent message with the nodes
-            #Update trye to update maybe trickled it???
+            for t in range(0,len(MESSAGE_rx)):
+                if ((MESSAGE_rx[t]-MESSAGE_RX_PREV[t]>0) and (NODE_index[t] not in CLIENT_add) and (NODE_index[t] not in DEST_add)):
+                    #This means the node received a message previously now forward it
+                    #This node then sends a hallo
+                    node_index = t
+                    #Define who it sends to
+                    #Define who it sends to
+                    #Local neighbour list
+                    local_neigh = NEIGH_add[node_index]
+                    tx = NODE_tx[node_index]
+                    rx = NODE_rx[node_index]
+                    #Sends a hallo to each
+                    #COunts as one message
+                    #Obtain the current traffic for the node
+                    traffic = tx+rx
+                    # TODO make this more general
+                    #Define the next hop
+                    target = DESTINATION_ROUTING_table_path[node_index][0][0]
+                    target_index = NODE_index.index(target)
+                    # Update my tx
+                    NODE_tx[node_index] += 1
+                    for p in range(0, len(NEIGH_tx[node_index])):
+                        #Because every neigh will get the packet just not repond to it
+                        NEIGH_tx[node_index][p] += 1
+                    if not (is_collide(collision(traffic * 8, 1))):
+                        # Message went through
+                        MESSAGE_rx[target_index]+=1
+                        NODE_rx[target_index] += 1
+                        if NODE_index[target_index] in DEST_add:
+                            DEST_MESSAGE_rx[DEST_add.index(NODE_index[target_index] )]+=1
+
+                        # Update rx
+                        rx_index = NEIGH_add[target_index].index(NODE_index[node_index])
+                        NEIGH_rx[target_index][rx_index] += 1
+            #Make a prev copy again
+            MESSAGE_RX_PREV = list(np.copy(MESSAGE_rx))
             #Initializes the amount for smallest possible time step
             if j in NODE_Hallo:
                 #This node then sends a hallo
                 node_index = NODE_Hallo.index(j)
+
                 #Define who it sends to
                 #Local neighbour list
                 local_neigh = NEIGH_add[node_index]
@@ -230,7 +270,7 @@ if __name__ == "__main__":
                 NODE_Hallo[NODE_Hallo.index(j)] = (NODE_Hallo[NODE_Hallo.index(j)]+ amount_symbols/hallo_freq)%amount_symbols
             if j in CLIENT_tx:
                 #This node then sends a hallo
-                node_index = CLIENT_tx.index(j)
+                node_index = NODE_index.index(CLIENT_add[CLIENT_tx.index(j)])
                 #Define who it sends to
                 #Define who it sends to
                 #Local neighbour list
@@ -253,7 +293,7 @@ if __name__ == "__main__":
                     NEIGH_tx[node_index][p] += 1
                 if not (is_collide(collision(traffic * 8, 1))):
                     # Message went through
-                    MESSAGE_rx[target_index]
+                    MESSAGE_rx[target_index]+=1
                     NODE_rx[target_index] += 1
                     # Update rx
                     rx_index = NEIGH_add[target_index].index(NODE_index[node_index])
@@ -269,30 +309,31 @@ if __name__ == "__main__":
             for l in range(0,len(NEIGH_tx[s])):
                 PACKETS_sent[s][l].append(NEIGH_tx[s][l])
         #Save packets received
-        for s in range(0,len(NEIGH_tx)):
-            for l in range(0,len(NEIGH_tx[s])):
-                PACKETS_sent[s][l].append(NEIGH_tx[s][l])
+        for s in range(0,len(NEIGH_rx)):
+            for l in range(0,len(NEIGH_rx[s])):
+                PACKETS_received[s][l].append(NEIGH_rx[s][l])
         for s in range(0,len(MESSAGE_tx)):
             Client_TX_Amount[s].append(MESSAGE_tx[s])
         #TODO MAKE THIS MORE DYNAMIC
-        Dest_RX_Amount[0].append(MESSAGE_rx[-1])
+        Dest_RX_Amount[0].append(DEST_MESSAGE_rx[0])
         #Now clear the trackers
         # STores amount messages sent per second uplink
+        print(NEIGH_tx)
         NEIGH_tx = [[0, 0], [0, 0, 0], [0, 0, 0], [0, 0]]
         # STores amount messages received per second downlink
         NEIGH_rx = [[0, 0], [0, 0, 0], [0, 0, 0], [0, 0]]
         # STores amount hallo received per second downlink
         # If this amount is not at least hallo freq then link breaks
         HALLO_rx = [[0, 0], [0, 0, 0], [0, 0, 0], [0, 0]]
-        # On next tick if the index to the right is lower then left then feed forward the message.
-        MESSAGE_rx = [0, 0, 0, 0]
         # Defines when client sent message
         MESSAGE_tx = [0]
+        DEST_MESSAGE_rx = [0]
         # Defines how many messages I actually sent
         NODE_tx = [0, 0, 0, 0]
         # Defines messages I actually received
         NODE_rx = [0, 0, 0, 0]
 
     print(LOAD_array)
-    print(NODE_rx)
-    print(NODE_tx)
+    print(PACKETS_sent)
+    print(PACKETS_received)
+    print(Dest_RX_Amount)
